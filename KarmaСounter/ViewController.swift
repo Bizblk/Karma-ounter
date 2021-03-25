723dc77ea0762ecb03d1b95992830bf716d842e4
Bizblk <enemy_counter@mail.ru>
2021-03-25 10:50:54 +0300

    add core data

--- KarmaСounter/ViewController.swift
+++ KarmaСounter/ViewController.swift
@@ -8,15 +8,32 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var karamaCount: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var enterNameLabel: UITextField!
+    @IBOutlet weak var saveNameButton: UIButton!
+    @IBOutlet weak var yourKarmaLabel: UILabel!
+    @IBOutlet weak var addKarmaButton: UIButton!
     
     private var count = 0
-    private var name = NSManagedObject()
+    private var toggle = false
+    var dataStoreManager = DataStoreManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        if userNameLabel.text == "Enter you name" {
+//            yourKarmaLabel.isHidden = true
+//            addKarmaButton.isHidden = true
+//            karamaCount.isHidden = true
+//
+//        } else {
+//            enterNameLabel.isHidden = true
+//            saveNameButton.isHidden = true
+//        }
+        
         karamaCount.text = String(count)
+        enterNameLabel.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -30,8 +47,20 @@ class ViewController: UIViewController {
         count = 0
         karamaCount.text = String(count)
     }
-    @IBAction func saveName(_ sender: Any) {
-        
+    @IBAction func saveName(_ sender: Any){
+        let name = enterNameLabel.text ?? "NO NAME"
+        let user = dataStoreManager.obtainMainUser(name: name)
+        userNameLabel.text = user.name
     }
+    
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        <#code#>
+//    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
